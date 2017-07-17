46b09681f38b2373945bee9230a6f8b9d6e70b82
stbui <w431106@163.com>
2017-07-17 12:59:48 +0800

    update

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomersComponent } from './customers.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CustomersComponent]
+})
+export class CustomersModule { }
