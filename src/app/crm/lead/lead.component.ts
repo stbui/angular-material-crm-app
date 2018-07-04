4d5dadbaa4533814030f77516b2635d301fdb4dd
stbui <w431106@163.com>
2018-07-04 21:09:35 +0800

    feat: 更新线索功能

--- src/app/crm/lead/lead.component.ts
+++ src/app/crm/lead/lead.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { NewComponent } from './new/new.component';
 
 @Component({
   selector: 'app-lead',
@@ -8,47 +10,49 @@ import { Component, OnInit, Inject } from '@angular/core';
 export class LeadComponent implements OnInit {
   data;
 
-  constructor(@Inject('LeadService') private _service) {}
+  rows: any[] = [];
+  loadingIndicator = true;
+  reorderable = true;
+
+  selected = [];
+
+  constructor(
+    @Inject('LeadService') private service,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
-    // this.select();
-    this.renderTable();
+    for (let i = 0; i < 50; i++) {
+      this.rows.push({
+        name: '姓名' + i,
+        company: '公司名称' + i,
+        tel: '电话' + i,
+        phone: '手机' + i,
+        status_mapped: '跟进状态' + i,
+        lastest_revisit_log: '最新跟进记录' + i,
+        real_revisit_at: '实际跟进时间' + i,
+        revisit_remind_at: '下次跟进时间' + i
+      });
+    }
 
-    this._service.getLead().then(res => {
-      console.log(res);
+    this.loadingIndicator = false;
+  }
+
+  onNew(e) {
+    let dialogRef = this.dialog.open(NewComponent, {
+      width: '500px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
     });
   }
 
-  select() {
-    this.data = {
-      columns: [
-        { key: 'id', title: 'id', type: 'selection' },
-        { key: 'name', title: '姓名' },
-        { key: 'name', title: '公司名称' },
-        { key: 'name', title: '电话' }
-      ],
-      data: []
-    };
-
-    return this.data;
+  onActivate(event) {
+    console.log('Activate Event', event);
   }
 
-  count() {}
-
-  /*
-   * 搜索事件
-   * */
-  onSearch(value) {}
-
-  onSelect($event) {}
-
-  onDelete() {}
-
-  // table 数据渲染
-  renderTable() {}
-
-  // 分页事件
-  onPage($event) {}
-
-  getPageTotal() {}
+  onSelect({ selected }) {
+    console.log('Select Event', selected, this.selected);
+    this.selected = selected;
+  }
 }
